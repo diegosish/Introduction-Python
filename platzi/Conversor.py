2baf07583d6a97c74ae130f541105099531af1a0
Diego Martínez <diegosish@gmail.com>
2022-01-18 16:45:25 -0500

    Conversor de Moneda

---
+++ platzi/Conversor.py
@@ -0,0 +1,6 @@
+Pesos = input("¿Cuántos Pesos Colombianos tiene?: ")
+Pesos = float(Pesos)
+v_Dolar = 4033
+Dolares = Pesos / v_Dolar
+Dolares = str(Dolares)
+print("Tienes $" + Dolares + " Dolares")
