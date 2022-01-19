caa7c65acd140cfd716bb51b86d91d3c1e1dff80
Diego Martínez <diegosish@gmail.com>
2022-01-19 17:18:42 -0500

    Condicionales.

---
+++ platzi/Condicionales.py
@@ -0,0 +1,19 @@
+edad = int(input("¿Cuántos años tienes?: "))
+if edad >= 50:
+    print("Se encuentra en la primera etapa de priorización")
+elif edad > 18 and edad <=30:
+    print("No se Encuentra Priorizado")
+elif edad > 30 and edad <=50:
+    print("Se encuentra en la segunda etapa de priorización")
+elif edad < 18:
+    print("No se Encuentra Priorizado") 
+
+
+# cc = int(input("Escribe tu número de identificación: "))
+
+# if cc > 9999:
+#     print("Se Encuentra en Proceso")
+# elif cc == 9999:
+#     print("Por Favor escriba un número valido")
+# else:
+#     print("No se encuentra registrado")
